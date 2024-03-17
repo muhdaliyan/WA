55ce5503a783c74c7fc86ae4243f504b15717f27
TeeOnez <90501348+TeeOnez@users.noreply.github.com>
2024-03-18 01:16:40 +0500

    abe ho jaw

--- sendex/views.py
+++ sendex/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import webbrowser
 
 def sender(request):
     if request.method=="POST":
@@ -9,29 +10,20 @@ def sender(request):
         phone_number = ph
         message = msg
 
-        open_whatsapp_chat(phone_number, message)
-        return redirect('https://web.whatsapp.com/')
+        phone_number = phone_number.strip("+-0")
+        phone_number = phone_number.replace(" ", "")
+        encoded_message = message.replace(" ", "%20")
+
+        if not phone_number.isdigit():
+          print("Invalid phone number. Please enter a valid number with digits only.")
+          return
+        
+        url = f"https://web.whatsapp.com/send?phone={phone_number}&text={encoded_message}"
+
+
+        webbrowser.open(url)
+        return redirect('/')
     
     pre_msg = "AOA! \nWe noticed [QubeSuit] doesn't have a website. Let's fix that! A professional website can attract more customers and boost credibility. \nCan we chat about how we can help this week? \nCheers, \nQubeknit 🚀"
     
     return render(request, 'index.html', {'prefilled_message': pre_msg})
-
-
-
-def open_whatsapp_chat(phone_number, message):
-  
-  phone_number = phone_number.strip("+-0")
-  phone_number = phone_number.replace(" ", "")
-  encoded_message = message.replace(" ", "%20")
-
-  if not phone_number.isdigit():
-    print("Invalid phone number. Please enter a valid number with digits only.")
-    return
-
-  url = f"https://web.whatsapp.com/send?phone={phone_number}&text={encoded_message}"
-
-  import webbrowser
-  webbrowser.open(url)
-
-  print(f"Opening WhatsApp chat with {phone_number}.\n"
-        f"Please review and edit the pre-filled message before sending.")
