dc39572b744b728faa0a716ca4843fb6002f00fc
TeeOnez <90501348+TeeOnez@users.noreply.github.com>
2024-03-18 01:25:53 +0500

    mana lay

--- sendex/views.py
+++ sendex/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
-import webbrowser
+from django.shortcuts import render, redirect, HttpResponseRedirect
 
 def sender(request):
-    if request.method=="POST":
+    if request.method == "POST":
         data = request.POST
 
         ph = data.get('ph')
@@ -10,20 +9,21 @@ def sender(request):
         phone_number = ph
         message = msg
 
-        phone_number = phone_number.strip("+-0")
-        phone_number = phone_number.replace(" ", "")
-        encoded_message = message.replace(" ", "%20")
+        return open_whatsapp_chat(phone_number, message)
 
-        if not phone_number.isdigit():
-          print("Invalid phone number. Please enter a valid number with digits only.")
-          return
-        else:
-           
-        
-          url = f"https://web.whatsapp.com/send?phone={phone_number}&text={encoded_message}"
-          webbrowser.open(url)
-        return redirect('/')
-    
-    pre_msg = "AOA! \nWe noticed [QubeSuit] doesn't have a website. Let's fix that! A professional website can attract more customers and boost credibility. \nCan we chat about how we can help this week? \nCheers, \nQubeknit 🚀"
-    
+    pre_msg = "AOA! \nWe noticed [QubeSuit] doesn't have a website. Let's fix that! A professional website can attract more customers and boost credibility. \nCan we chat about how we can help this week? \nCheers, \nQubeknit "
+
     return render(request, 'index.html', {'prefilled_message': pre_msg})
+
+
+def open_whatsapp_chat(phone_number, message):
+    phone_number = phone_number.strip("+-0")
+    phone_number = phone_number.replace(" ", "")
+    encoded_message = message.replace(" ", "%20")
+
+    if not phone_number.isdigit():
+        print("Invalid phone number. Please enter a valid number with digits only.")
+        return
+
+    url = f"https://web.whatsapp.com/send?phone={phone_number}&text={encoded_message}"
+    return HttpResponseRedirect(url)
